22fcb9105ccab30c279b81fdc5589edcd4e7f60c
Seungcode <104374372+Seungcode@users.noreply.github.com>
2023-11-25 19:45:29 +0900

    Delete AndroidStudioProjects/Todo2 directory

--- AndroidStudioProjects/Todo2/app/src/main/java/com/example/todo2/TodoEntity.kt
+++
@@ -1,9 +0,0 @@
-package com.example.todo2
-
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-@Entity(tableName = "todo")
-data class TodoEntity(@PrimaryKey(autoGenerate = true) var id: Int?,
-var description: String ="",
-var date:String ="")
